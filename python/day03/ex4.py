d31f9e834efea33b4df920451e6eae8f7d162837
vijaykumarreddybandlapalli <vijaykumarredybandlapalli@gmail.com>
2024-11-11 21:53:17 +0530

    python

--- python/day03/ex4.py
+++
@@ -1,5 +0,0 @@
-l1=['jeevenreddy','vijay','reddymohan']
-for i in l1:
-    if len(i)>6:
-        continue
-    print(i)
---
+++ python_new/day01(conversion)/ex1.py
@@ -0,0 +1,8 @@
+x:int=20
+print(x)
+print(type(x))
+
+
+y:float=20.211
+print(y)
+print(type(y))
